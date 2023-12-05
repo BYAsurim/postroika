eca2d4ea322ba559d213dad490b0aeb6964753f9
BYAsurim <Pers0naL276276@gmail.com>
2023-12-05 19:05:56 +0300

    add react-router-dom and components AboutUs.tsx, Home.tsx, Projects.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import s from './App.module.css';
-import Navbar from "./navbar/Navbar";
-import {HomePage} from "./main/HomePage";
-import {Reputation} from "./reputation/Reputation";
+import Home from "./home/Home";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {Contacts} from "./contacts/Contacts";
+import {Projects} from "./projects/Projects";
+import {AboutUs} from "./aboutUs/AboutUs";
 
 
 function App() {
     return (
         <div className={s.App}>
-            <Navbar/>
-            <HomePage/>
-            <Reputation/>
-            <Contacts/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<AboutUs/>}/>
+                <Route path='/projects' element={<Projects/>}/>
+                <Route path='/contact' element={<Contacts/>}/>
+                <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
+                <Route path='*' element={<Navigate to={'/404'}/>}/>
+            </Routes>
         </div>
     );
 }
---
+++ src/home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from "../navbar/Navbar";
+import {HomePage} from "../main/HomePage";
+import {Reputation} from "../reputation/Reputation";
+import {Contacts} from "../contacts/Contacts";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <HomePage/>
+            <Reputation/>
+            <Contacts/>
+        </div>
+    );
+};
+
+export default Home;
