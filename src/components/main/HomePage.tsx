b6a9d97896c236760f0b5bd0e15682477c614c6f
BYAsurim <Pers0naL276276@gmail.com>
2024-03-28 17:02:56 +0300

    refactor homePage

--- src/components/main/HomePage.tsx
+++ src/components/main/HomePage.tsx
@@ -4,16 +4,19 @@ import s from './HomePage.module.css'
 
 export const HomePage = () => {
     return (
+
         <div className={s.backgroundImg}>
-            <h2 className={s.h2}>
-                Реализуем любые проекты под ваш заказ!
-            </h2>
-            <div className={s.text}>
-                Услуги маляра,
-                покраска стен,
-                декоративная покраска стен,
-                нанесение жидких обоев,
-                покраска потолка и др.
+            <div className={s.container}>
+                <h1 className={s.h1}>
+                    Профессиональная покраска и штукатурка стен в минске
+                </h1>
+                <h2 className={s.text}>
+                    Услуги маляра,
+                    покраска стен,
+                    декоративная покраска стен,
+                    нанесение жидких обоев,
+                    покраска потолка и др.
+                </h2>
             </div>
         </div>
     );
